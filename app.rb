461bfeb6b93c99b923c49f0f976d39c3bd7ce084
Tom Close <tom.close@cantab.net>
2013-08-06 17:04:58 +0100

    Try different mongo heroku setup

--- app.rb
+++ app.rb
@@ -5,17 +5,8 @@ require 'json'
 ## Mongoid setup
 ## =============
 
-configure do
-    Mongoid.configure do |config|
-       if ENV['MONGOHQ_URL']
-        conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-        uri = URI.parse(ENV['MONGOHQ_URL'])
-        config.master = conn.db(uri.path.gsub(/^\//, ''))
-      else
-        Mongoid.load!("mongoid.yml", :development)
-      end
-    end
-end
+Mongoid.load!("mongoid.yml")
+     
 
 
 
