380806932385a3c4494e72ca2afc4201087ffb46
Tom Close <tom.close@cantab.net>
2013-08-06 16:48:59 +0100

    Basic app working

--- app.rb
+++ app.rb
@@ -19,14 +19,20 @@ end
 
 # display signup form
 get '/' do
-    
+    erb :form
 end
 
 # add new user, display thanks
 post '/' do
-
+    name = params[:name]
+    email = params[:email]
+    @user = User.new(:name => name, :email => email)
+    @user.save
+    erb :thanks
 end
 
 # show all the users so far
 get '/list' do
+    @users = User.all
+    erb :list
 end
