d2123df8bf8f016fb9195608f8745e3fd6f34146
Tom Close <tom.close@cantab.net>
2013-08-08 13:04:12 +0100

    Email now using a mail template

--- example_app/app.rb
+++ example_app/app.rb
@@ -17,18 +17,18 @@ end
 ## Email setup
 ## ===========
 
-Pony.options = { :via => 'smtp',
-                 :via_options => {
-                                :address              => 'smtp.gmail.com',
-                                :port                 => '587',
-                                :enable_starttls_auto => true,
-                                :user_name            => ENV['USER_NAME'],
-                                :password             => ENV['PASSWORD'],
-                                :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-                                :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-
-                 }
-                }
+Pony.options = { 
+  :via => 'smtp',
+  :via_options => {
+      :address              => 'smtp.gmail.com',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => ENV['USER_NAME'],
+      :password             => ENV['PASSWORD'],
+      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+      :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+    }
+  }
 
 
 ## Sinatra app
@@ -40,11 +40,11 @@ get '/' do
 end
 
 post '/' do
-  name = params[:fruit_name]
+  @name = params[:fruit_name]
   email = params[:email]
-  f = Fruit.new(:name => name)
+  f = Fruit.new(:name => @name)
   f.save
-  Pony.mail(:to => email, :subject => "Congratulations, you added a fruit!", :body => "#{name} is a great fruit.")
+  Pony.mail(:to => email, :subject => "Congratulations, you added a fruit!", :body => erb(:email, :layout => false))
   @fruits = Fruit.all
   erb :index
 end
