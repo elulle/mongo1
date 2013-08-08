ab1287959ac2f6c984a8a857d43cc40b1593b7c2
Tom Close <tom.close@cantab.net>
2013-08-08 12:37:02 +0100

    Example app sending emails

--- example_app/app.rb
+++ example_app/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'mongoid'
+require 'pony'
 require 'json'
 
 ## Mongoid setup
@@ -13,6 +14,23 @@ class Fruit
   field :name
 end
 
+## Email setup
+## ===========
+
+Pony.options = { :via => 'smtp',
+                 :via_options => {
+                                :address              => 'smtp.gmail.com',
+                                :port                 => '587',
+                                :enable_starttls_auto => true,
+                                :user_name            => ENV['USER_NAME'],
+                                :password             => ENV['PASSWORD'],
+                                :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+                                :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+
+                 }
+                }
+
+
 ## Sinatra app
 ## ===========
 
@@ -23,8 +41,10 @@ end
 
 post '/' do
   name = params[:fruit_name]
+  email = params[:email]
   f = Fruit.new(:name => name)
   f.save
+  Pony.mail(:to => email, :subject => "Congratulations, you added a fruit!", :body => "#{name} is a great fruit.")
   @fruits = Fruit.all
   erb :index
 end
